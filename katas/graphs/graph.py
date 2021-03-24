d476ccbdf97049d18dbbeca636025e3c716cb543
Vishwanath Sangale <vishsangale@gmail.com>
2021-03-24 15:29:42 -0700

    Code Katas: Basic implementation of undirected graph node

---
+++ katas/graphs/graph.py
@@ -0,0 +1,17 @@
+
+class GraphNode(object):
+    def __init__(self):
+        self.val = None
+        self.children = set()
+
+    def add_edge(self, node):
+        self.children.add(node)
+
+    def remove_edge(self, node):
+        if node in self.children:
+            self.children.remove(node)
+
+    def get_childs(self):
+        return self.children
+
+
